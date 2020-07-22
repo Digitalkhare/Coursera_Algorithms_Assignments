066cd055147c0ffc7ebf556c7cd5a8dc3fa451eb
osomokhare <osomokare@yahoo.com>
2020-07-22 15:44:07 +0100

    Permutation implementation from assignment coursera algorithms lecture

---
+++ src/mygithubprojects/Permutation.java
@@ -0,0 +1,23 @@
+package mygithubprojects;
+
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+        int readInt = Integer.parseInt(args[0]);
+        while (!StdIn.isEmpty()) {
+            randomizedQueue.enqueue(StdIn.readString());
+        }
+        for (int i = 0; i < readInt; i++) {
+            StdOut.println(randomizedQueue.dequeue());
+        }
+    }
+    
+}
